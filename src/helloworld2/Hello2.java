a5f766c3b2739bd2b5db346dce29c2229c2c3e29
ewg3@students.ptcollege.edu <>
2019-02-07 14:28:25 -0500

    this is a first comment

---
+++ src/helloworld2/Hello2.java
@@ -0,0 +1,19 @@
+//FilenameHello2.java
+//Written by Erik Garrow
+ //Written on 10/22/2018
+package helloworld2;
+
+/**
+ *
+ * @author erikg
+ */
+public class Hello2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
