44c2961cb6e92ef962154489388ad312de7a902c
ewg3@students.ptcollege.edu <>
2019-02-07 13:50:05 -0500

    this is a first comment

---
+++ src/garrow/Garrow.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package garrow;
+
+/**
+ *
+ * @author erikg
+ */
+public class Garrow {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Garrow();
+    }
+    public static void Garrow()
+    {
+        System.out.println("Erik Garrow!");
+    }
+    
+    
+}
